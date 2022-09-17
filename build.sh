ea5078c4184510c4dfcc5b255b4025636c75a41b
Jerry <82269916@qq.com>
2022-09-17 16:17:51 +0800

    Init

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker pull  --platform=linux/arm64 jerry9916/centos-box:latest
-docker build --platform=linux/arm64 -t jerry9916/hadoop-hive:arm64 .
+docker build --platform=linux/arm64 -t jerry9916/hadoop-hive:arm64_v3 .
